c87f9ca83a108b0f14a8b2421edb936440252c75
ricardobuuueno <ricardo.bueno.sf.dev@hotmail.com>
2022-08-22 16:44:10 -0400

    First commit

---
+++ scanner.py
@@ -0,0 +1,38 @@
+#!/bin/python
+
+import sys
+import socket
+from datetime import datetime
+
+#Define our target
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: python3 scanner.py <ip>")
+
+#Add a pretty banner
+print("-" * 50)
+print("Scanning target " + target)
+print("Time started: " + str(datetime.now()))
+print("-" * 50)
+
+try:
+    for port in range(50,85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port)) #returns an error indicator
+        if result == 0:
+            print(f"Port {port} is open")
+        s.close()
+except KeyboardInterrupt:
+    print("\nExiting program.")
+    sys.exit()
+
+except socket.gaierror:
+    print("\nHostname could not be resolved.")
+    sys.exit()
+
+except socket.error:
+    print("\nCouldn't connect to server.")
+    sys.exit()
